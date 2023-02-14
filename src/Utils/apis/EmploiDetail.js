6f37a9ecda3aed2f39117485c99f2e668a657d93
mael-mtn <mantuamael14@gmail.com>
2023-02-14 12:18:37 +0100

    Probleme de chemin pour les images emploi  corrige

--- src/Utils/apis/EmploiDetail.js
+++ src/Utils/apis/EmploiDetail.js
@@ -1,37 +1,43 @@
 const EmploiDetail = [
   {
     id: "1",
-    Image: "../../Images/Emploi/animation.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1591261730588-9c21770ff6f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Fast-Food",
     linkCat: "",
   },
   {
     id: "2",
-    Image: "../../Images/Emploi/logistique.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1587293852726-70cdb56c2866?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Logistique",
     linkCat: "",
   },
   {
     id: "3",
-    Image: "../../Images/Emploi/interim.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1582845703600-238f8f867614?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Grande Distribution",
     linkCat: "",
   },
   {
     id: "4",
-    Image: "../../Images/Emploi/restauration.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1591261730588-9c21770ff6f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Restauration",
     linkCat: "",
   },
   {
     id: "5",
-    Image: "../../Images/Emploi/animation.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1636717944778-bb21009c3fb4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Culturel",
     linkCat: "",
   },
   {
     id: "6",
-    Image: "../../Images/Emploi/interim.jpg",
+    Image:
+      "https://images.unsplash.com/photo-1582845703600-238f8f867614?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHx2aXN1YWwtc2VhcmNofDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
     title: "Interim",
     linkCat: "",
   },
