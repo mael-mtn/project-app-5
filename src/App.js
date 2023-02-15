cfca7755e64abc2303b4a9515e396a7ec48d169c
mael-mtn <mantuamael14@gmail.com>
2023-02-15 12:34:01 +0100

    Creation de la pages d'accueil et prepa routes

--- src/App.js
+++ src/App.js
@@ -1,102 +1,13 @@
 import "./App.css";
-import { motion } from "framer-motion";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, Fragment } from "react";
 import Header from "./Components/Header/Header";
-import Propert from "./Components/Cards/CardsLogement/Propert";
-import CardsEmploi from "./Components/Cards/CardsEmploi/CardsEmploi";
-import CardsAide from "./Components/Cards/CardsAide/CardsAide";
-import SearchBar from "./Components/searchBar/SearchBar";
 
 function App() {
-  const [width, setWidth] = useState(0);
-  const carousel = useRef();
-
-  useEffect(() => {
-    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
-  }, []);
-
   return (
-    <main>
+    <Fragment>
       <Header />
-
-      <section className="profil">
-        <div className="presentation">
-          <div class="circular--portrait">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzHQv_th9wq3ivQ1CVk7UZRxhbPq64oQrg5Q&usqp=CAU"
-              width={"100%"}
-              height={"100%"}
-              alt="sdv"
-            />
-          </div>
-          <span className="name">Hello John !</span>
-        </div>
-        <div className="searchBar">
-          <p>Voici les dernières nouveautés !</p>
-          <SearchBar/>
-        </div>
-      </section>
-
-      <motion.div
-        ref={carousel}
-        className="carousel"
-        whileTap={{ cursor: "grabbing" }}
-      >
-        <div className="left">
-          <h3>Logements</h3>
-          <span>Voir plus (nb)</span>
-        </div>
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-Carousel"
-        >
-          <motion.div className="item">
-            <Propert className="propert" />
-          </motion.div>
-        </motion.div>
-      </motion.div>
-
-      <motion.div
-        ref={carousel}
-        className="carousel"
-        whileTap={{ cursor: "grabbing" }}
-      >
-        <div className="left-emploi">
-          <h3>Emplois</h3>
-          <span>Voir plus (nb)</span>
-        </div>
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-Carousel"
-        >
-          <motion.div className="item">
-            <CardsEmploi className="emploi" />
-          </motion.div>
-        </motion.div>
-      </motion.div>
-
-      <motion.div
-        ref={carousel}
-        className="carousel"
-        whileTap={{ cursor: "grabbing" }}
-      >
-        <div className="left-aide">
-          <h3>Aides</h3>
-          <span>Voir plus (nb)</span>
-        </div>
-        <motion.div
-          drag="x"
-          dragConstraints={{ right: 0, left: -width }}
-          className="inner-Carousel"
-        >
-          <motion.div className="item">
-            <CardsAide className="aide" />
-          </motion.div>
-        </motion.div>
-      </motion.div>
-    </main>
+      
+    </Fragment>
   );
 }
 
